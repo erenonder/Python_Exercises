dedf9284c05c728401c7561a486a99fff5acc6c3
Onder Eren <ondereren83@gmail.com>
2020-06-14 15:22:33 +0200

    Updated first file for test purpose

--- first.py
+++ first.py
@@ -6,5 +6,6 @@ comp = "ASML"
 
 mystr = f"{name} works for {comp}"
 print(mystr)
-#print("{} works for {}".format(name,comp))
+# print("{} works for {}".format(name,comp))
 print(sys.version)
+print(mystr[4:0:-1])
